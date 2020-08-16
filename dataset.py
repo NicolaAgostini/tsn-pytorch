089b96fb32aa8779dd4769e818f4309d443be7cc
nicolago <vodafonenicola@yahoo.it>
2020-08-16 19:05:07 +0200

    some prints2

--- dataset.py
+++ dataset.py
@@ -79,7 +79,7 @@ class TSNDataSet(data.Dataset):
             offsets = np.sort(randint(record.num_frames - self.new_length + 1, size=self.num_segments))
         else:
             offsets = np.zeros((self.num_segments,))
-        print(record.path + " " + str(record.start_fr) + "  " + str(offsets)+ "  "+ str(record.label))
+        print(record.path + " " + str(record.start_fr) + "  " + str(offsets)+ "  "+ str(record.label)+ str(offsets+record.start_fr))
 
 
         return offsets + 1
